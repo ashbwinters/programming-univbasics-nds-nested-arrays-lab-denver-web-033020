701ed5890456a21690f4f71b3563708727e7cc55
Ashley Winters <ashbwinters@gmail.com>
2020-03-16 17:27:11 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -18,13 +18,13 @@ CONVENTIONAL_PRODUCE = [
 
 def assembled_matrix
   produce = [
-    [CONVENTIONAL_PRODUCE],
-    [ORGANIC_PRODUCE]
+    CONVENTIONAL_PRODUCE,
+    ORGANIC_PRODUCE
     ]
 end
 
 def sorted_matrix
-  sorted_produce = assembled_matrix.sort
+  assembled_matrix.sort!
 end
 
 def matrix_lookup(matrix, row, column)
